411f1d97bd5d2e0c24b92c75cf7ff98f676c7d49
shemeshg <gili.shemesh@gmail.com>
2025-01-05 15:39:33 +0200

    commit

--- src/musicFormat.ts
+++ src/musicFormat.ts
@@ -1,50 +1,6 @@
 
 
-function getBarPositions(line: string): number[] {
-    let positions: number[] = [];
 
-    for (let i = 0; i < line.length; i++) {
-        if (line[i] === '|') {
-            positions.push(i);
-        }
-    }
-
-    return positions;
-}
-
-function barsContent(line: string): string[] {
-    const positions = getBarPositions(line);
-    let ret = [];
-    for (let i = 0; i < positions.length - 1; i++) {
-        const barContent = line.substring(positions[i] + 1, positions[i + 1]);
-        ret.push(barContent);
-    }
-    return ret;
-}
-
-function barItems(barString: string): string[] {
-
-
-    let result = barString.match(/\([^)]*\)|\S+/g);
-    if (result) {
-        return result;
-    }
-    return [];
-}
-
-function padLR(text: string, requestedLen: number) {
-    // Calculate the total padding needed
-    let totalPadding = requestedLen - text.length;
-
-    // Calculate padding for left and right
-    let leftPadding = Math.floor(totalPadding / 2);
-    let rightPadding = totalPadding - leftPadding;
-
-    // Pad the text
-    let paddedText = ' '.repeat(leftPadding) + text + ' '.repeat(rightPadding);
-
-    return paddedText;
-}
 
 class FieldsClass {
     forId = 0;
@@ -53,81 +9,119 @@ class FieldsClass {
     totalLen = 0;
 }
 
-function spredEven(ary: string[], requestedLen: number, fc: FieldsClass, applayFromFieldClass: boolean): string {
-    let lenOfAllItemsInAry = ary.reduce((total, item) => total + item.length, 0);
-    let s = "";
-    let spacePerItem = Math.floor(requestedLen / (ary.length));
-
-    s = " ";
-
-    let forId = 0;
-    ary.forEach(element => {
-        // handel pharenses
-        let isItEnclosedWithParentheses = element.startsWith('(') && element.endsWith(')');
-        if (isItEnclosedWithParentheses) {
-            let contentInParentheses = element.slice(1, -1);
 
 
-            let fcChild: FieldsClass = new FieldsClass();
-            fcChild.forId = forId;
-            // if not already exists
-            const fcFound = fc.fcs.filter((fcItm) => {
-                return fcItm.forId === forId;
-            });
-            if (fcFound.length) {
-                fcChild = fcFound[0];
-            } else {
-                fc.fcs.push(fcChild);
-            }
-
-            let parenthesLen = contentInParentheses.length - 2;
-   
-            parenthesLen = 0;
-               
-            element = "(" + spredEven(barItems(contentInParentheses), parenthesLen, fcChild, applayFromFieldClass) + ")";
-
-        }
-
-        let elementToAdd = "";
-        if (applayFromFieldClass) {            
-            elementToAdd = padLR(element, fc.lens[forId]);
-        } else {
-            if (spacePerItem <= element.length) {
-                elementToAdd = " " + element + " ";
-            } else {                
-                elementToAdd = padLR(element, spacePerItem);
-            }
-            if (fc.lens[forId]) {
-                if (fc.lens[forId] < elementToAdd.length) {
-                    fc.lens[forId] = elementToAdd.length;
+class SpredEven {
+    public spredEven(barString: string, requestedLen: number, fc: FieldsClass, applayFromFieldClass: boolean): string {
+        return this._spredEven(this.barItems(barString), requestedLen, fc, applayFromFieldClass);
+    }
+    private _spredEven(ary: string[], requestedLen: number, fc: FieldsClass, applayFromFieldClass: boolean): string {
+        let lenOfAllItemsInAry = ary.reduce((total, item) => total + item.length, 0);
+        let s = "";
+        let spacePerItem = Math.floor(requestedLen / (ary.length));
+    
+        s = " ";
+    
+        let forId = 0;
+        ary.forEach(element => {
+            // handel pharenses
+            let isItEnclosedWithParentheses = element.startsWith('(') && element.endsWith(')');
+            if (isItEnclosedWithParentheses) {
+                let contentInParentheses = element.slice(1, -1);
+    
+    
+                let fcChild: FieldsClass = new FieldsClass();
+                fcChild.forId = forId;
+                // if not already exists
+                const fcFound = fc.fcs.filter((fcItm) => {
+                    return fcItm.forId === forId;
+                });
+                if (fcFound.length) {
+                    fcChild = fcFound[0];
+                } else {
+                    fc.fcs.push(fcChild);
                 }
+    
+                let parenthesLen = contentInParentheses.length - 2;
+       
+                parenthesLen = 0;
+                   
+                element = "(" + this._spredEven(this.barItems(contentInParentheses), parenthesLen, fcChild, applayFromFieldClass) + ")";
+    
+            }
+    
+            let elementToAdd = "";
+            if (applayFromFieldClass) {            
+                elementToAdd = this.padLR(element, fc.lens[forId]);
             } else {
-                fc.lens.push(elementToAdd.length);
+                if (spacePerItem <= element.length) {
+                    elementToAdd = " " + element + " ";
+                } else {                
+                    elementToAdd = this.padLR(element, spacePerItem);
+                }
+                if (fc.lens[forId]) {
+                    if (fc.lens[forId] < elementToAdd.length) {
+                        fc.lens[forId] = elementToAdd.length;
+                    }
+                } else {
+                    fc.lens.push(elementToAdd.length);
+                }
+            }
+    
+    
+            s = s + elementToAdd;
+            forId++;
+        });
+    
+        if (s.length < requestedLen) {        
+            s = this.padLR(s, requestedLen);
+        }
+    
+        if (applayFromFieldClass) {  
+            // if re-run required, bug 
+            if (fc.totalLen < s.length) {
+                fc.totalLen = s.length;           
+            } 
+            s = this.padLR(s, fc.totalLen );
+        } else {
+            if (fc.totalLen < s.length) {
+                fc.totalLen = s.length;
             }
         }
-
-
-        s = s + elementToAdd;
-        forId++;
-    });
-
-    if (s.length < requestedLen) {        
-        s = padLR(s, requestedLen);
+        return s;
     }
 
-    if (applayFromFieldClass) {  
-        // if re-run required, bug 
-        if (fc.totalLen < s.length) {
-            fc.totalLen = s.length;           
-        } 
-        s = padLR(s, fc.totalLen );
-    } else {
-        if (fc.totalLen < s.length) {
-            fc.totalLen = s.length;
+
+    
+
+    
+    private padLR(text: string, requestedLen: number) {
+        // Calculate the total padding needed
+        let totalPadding = requestedLen - text.length;
+    
+        // Calculate padding for left and right
+        let leftPadding = Math.floor(totalPadding / 2);
+        let rightPadding = totalPadding - leftPadding;
+    
+        // Pad the text
+        let paddedText = ' '.repeat(leftPadding) + text + ' '.repeat(rightPadding);
+    
+        return paddedText;
+    }
+
+
+    private barItems(barString: string): string[] {
+    
+    
+        let result = barString.match(/\([^)]*\)|\S+/g);
+        if (result) {
+            return result;
         }
+        return [];
     }
-    return s;
 }
+
+
 
 enum LineType {
     MusicBlockData,
@@ -197,7 +191,31 @@ class FormatMusicBlock {
             );
         return newLines;
     }
+
+    private getBarPositions(line: string): number[] {
+        let positions: number[] = [];
     
+        for (let i = 0; i < line.length; i++) {
+            if (line[i] === '|') {
+                positions.push(i);
+            }
+        }
+    
+        return positions;
+    }
+    
+    private barsContent(line: string): string[] {
+        const positions = this.getBarPositions(line);
+        let ret = [];
+        for (let i = 0; i < positions.length - 1; i++) {
+            const barContent = line.substring(positions[i] + 1, positions[i + 1]);
+            ret.push(barContent);
+        }
+        return ret;
+    }
+
+
+
     private pareseLines(applayFromFieldClass: boolean): LineObj[] {
         let retLines: LineObj[] = [];
         this._lines.forEach((valLine, idxLine) => {
@@ -205,14 +223,15 @@ class FormatMusicBlock {
        
             let lineObj:LineObj = new LineObj(valLine);
             if (lineObj.lineType === LineType.MusicBlockData){
-                barsContent(valLine).forEach((valBar, idxBar) => {
+                this.barsContent(valLine).forEach((valBar, idxBar) => {
                     let fcBar = new FieldsClass();
                     if (this._fcBars[idxBar]) {
                         fcBar = this._fcBars[idxBar];
                     } else {
                         this._fcBars.push(fcBar);
                     }
-                    const barText = spredEven(barItems(valBar), 0, fcBar, applayFromFieldClass);
+                    let se = new SpredEven();
+                    const barText = se.spredEven(valBar, 0, fcBar, applayFromFieldClass);
                     lineObj.musicBars.push(barText);
         
                 });
@@ -232,8 +251,8 @@ class FormatMusicBlock {
 function formatMusicBloc(bloack: string): string{
     let f: FormatMusicBlock = new FormatMusicBlock(bloack);
     f.doParse(false); //compact
-    f.doParse(true); //calculate apllay apaces
-    f.doParse(true); //calculate apllay apaces
+    f.doParse(true); //calc 
+    f.doParse(true); //recalc and apply
 
     return f.result;
 }
